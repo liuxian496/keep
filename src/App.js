5af623cc7032dac3881edbb2ef7d460d2c46ec93
Ford White <liuxian494@163.com>
2017-12-17 22:04:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react';
+import Apple from './Apple.js'
 
+class App extends Component {
+
+    componentDidMount(){
+    }
+
+    render() {
+        return (
+            <div>
+            <span>React unit test</span>
+            <Apple />
+            </div>
+        );
+    }
+}
+
+export default App;
